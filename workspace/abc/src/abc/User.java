fc5795a341844a55c2d21dd40a078cd536f9bc77
wsha <wsha@CSL4WK43.win2k.cs.ust.hk>
2015-03-04 13:34:42 +0800

    Add Blog Class

--- workspace/abc/src/abc/User.java
+++ workspace/abc/src/abc/User.java
@@ -11,19 +11,46 @@ public class User {
 		this.mail=mail;
 	}
 	
-	public boolean equals(Object o){
-		boolean ans = true;
-		o.getClass();
-		User user = (User) o;
-		if (user.id!=this.id)
-			ans = false;
-		if (user.code!=this.code)
-			ans = false;
-		if (user.mail!=this.mail)
-			ans = false;
-		return ans;
-	}
+
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((mail == null) ? 0 : mail.hashCode());
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		if (id != other.id)
+			return false;
+		if (mail == null) {
+			if (other.mail != null)
+				return false;
+		} else if (!mail.equals(other.mail))
+			return false;
+		return true;
+	}
+
+
+
 	public String toString(){
 		return("User [userId="+id+", userName="+code+", userEmail="+mail);
 	}
